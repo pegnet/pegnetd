4012c82d4295338d447d78eb529aadd8be92f255
Emyrk <stevenmasley@gmail.com>
2019-10-04 18:08:50 -0500

    Set activation height for oct 7, 15 UTC

--- node/node.go
+++ node/node.go
@@ -17,7 +17,10 @@ var OPRChain = *factom.NewBytes32FromString("a642a8674f46696cc47fdb6b65f9c87b2a1
 var TransactionChain = *factom.NewBytes32FromString("cffce0f409ebba4ed236d49d89c70e4bd1f1367d86402a3363366683265a242d")
 var PegnetActivation uint32 = 206421
 var GradingV2Activation uint32 = 210330
-var TransactionConversionActivation uint32 = 500000 // TODO: Fill this out to turn on tx/conv
+
+// TransactionConversionActivation indicates when tx/conversions go live on mainnet.
+// Target Activation Height is Oct 7, 2019 15 UTC
+var TransactionConversionActivation uint32 = 213237
 
 type Pegnetd struct {
 	FactomClient *factom.Client
