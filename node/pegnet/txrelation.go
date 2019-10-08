665a2c1209cf488c5e44fa58aa85bf2fe7f9913f
Emyrk <stevenmasley@gmail.com>
2019-10-08 13:36:31 -0500

    Switch from address_id to address
    The result_id was incorrect, and the address is sufficient for
    our purposes

--- node/pegnet/txrelation.go
+++ node/pegnet/txrelation.go
@@ -15,16 +15,16 @@ import (
 // If the transaction is a conversion, both "to" and "conversion" are set to true
 const createTableTransactions = `CREATE TABLE IF NOT EXISTS "pn_address_transactions" (
         "entry_hash"    BLOB NOT NULL,
-        "address_id"    INTEGER NOT NULL,
+        "address"       BLOB NOT NULL,
         "tx_index"      INTEGER NOT NULL,
         "to"            BOOL NOT NULL,
         "conversion"    BOOL NOT NULL,
 
-        PRIMARY KEY("entry_hash", "address_id"),
+        PRIMARY KEY("entry_hash", "address"),
 
-        FOREIGN KEY("address_id") REFERENCES "pn_addresses"
+        FOREIGN KEY("address") REFERENCES "pn_addresses"
 );
-CREATE INDEX IF NOT EXISTS "idx_address_transactions_address_id" ON "pn_address_transactions"("address_id");
+CREATE INDEX IF NOT EXISTS "idx_address_transactions_address" ON "pn_address_transactions"("address");
 `
 
 // InsertTransactionRelation inserts a row into "pnaddress_transactions" relating
@@ -33,14 +33,14 @@ CREATE INDEX IF NOT EXISTS "idx_address_transactions_address_id" ON "pn_address_
 // returned.
 //
 // If isConversion is true, the to field will automatically be set to true.
-func (p *Pegnet) InsertTransactionRelation(tx *sql.Tx, adrID int64, entryHash *factom.Bytes32, txIndex uint64, to bool, isConversion bool) (int64, error) {
+func (p *Pegnet) InsertTransactionRelation(tx *sql.Tx, adr factom.FAAddress, entryHash *factom.Bytes32, txIndex uint64, to bool, isConversion bool) (int64, error) {
 	stmt, err := tx.Prepare(`INSERT INTO "pn_address_transactions"
-                ("entry_hash", "address_id", "tx_index", "to", "conversion") VALUES
+                ("entry_hash", "address", "tx_index", "to", "conversion") VALUES
                 (?, ?, ?, ?, ?)`)
 	if err != nil {
 		return -1, err
 	}
-	res, err := stmt.Exec(entryHash[:], adrID, txIndex, to || isConversion, isConversion)
+	res, err := stmt.Exec(entryHash[:], adr[:], txIndex, to || isConversion, isConversion)
 	if err != nil {
 		return -1, err
 	}
