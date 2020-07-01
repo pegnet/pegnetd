f6ad0069f1c52a92e0a049ba309d8c2625adabd7
code maniac <koneits@outlook.com>
2020-07-01 14:29:20 +0200

    V2.0 Height Activation
    Indicates the activation of PegNet 2.0

--- node/node.go
+++ node/node.go
@@ -50,6 +50,10 @@ var (
 	// V4OPRUpdate indicates the activation of additional currencies and ecdsa keys.
 	// Estimated to be  Feb 12, 2020, 18:00 UTC
 	V4OPRUpdate uint32 = 231620
+	
+	// V20HeightActivation indicates the activation of PegNet 2.0.
+	// Estimated to be  XXXX XXXX XXXX
+	V20HeightActivation int64 = 999999
 )
 
 func SetAllActivations(act uint32) {
