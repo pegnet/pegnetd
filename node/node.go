8ec64b81695462a727712cc05269d13b148b453c
StarNeit <koneits@outlook.com>
2020-07-14 15:12:11 +0200

    replace V20HeightActivation as real value

--- node/node.go
+++ node/node.go
@@ -50,10 +50,10 @@ var (
 	// V4OPRUpdate indicates the activation of additional currencies and ecdsa keys.
 	// Estimated to be  Feb 12, 2020, 18:00 UTC
 	V4OPRUpdate uint32 = 231620
-	
+
 	// V20HeightActivation indicates the activation of PegNet 2.0.
-	// Estimated to be  XXXX XXXX XXXX
-	V20HeightActivation uint32 = 999999
+	// Estimated to be  July 29th 2020 16:40 UTC
+	V20HeightActivation uint32 = 255778
 )
 
 func SetAllActivations(act uint32) {
