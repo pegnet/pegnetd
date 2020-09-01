51c7dadb4bfa55b718fdbeee3d1105973927255b
StarNeit <koneits@outlook.com>
2020-09-01 21:19:43 +0300

    new branch

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-go install -ldflags="-X github.com/pegnet/pegnetd/config.CompiledInBuild=`git rev-parse HEAD` -X github.com/pegnet/pegnetd/config.CompiledInVersion=`git describe --tags`"
--- fat/fat2/doc.go
+++
@@ -1,4 +0,0 @@
-// Package fat2 provides data types corresponding to valid FAT-2 entries for
-// PegNet Transactions and Conversions as well as methods for validating the
-// structure and content of the factom entry.
-package fat2
