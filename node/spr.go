f462a5ce4d0284d3c106f5b6c91dc859026541b0
dragonheaven <happyfamilylife11@gmail.com>
2020-09-15 22:51:42 +0300

    version 7

--- node/spr.go
+++ node/spr.go
@@ -5,20 +5,7 @@ import (
 	"fmt"
 	"github.com/Factom-Asset-Tokens/factom"
 	"github.com/pegnet/pegnet/modules/graderStake"
-	"math/rand"
-	"time"
 )
-
-func ShuffleEntries(entries []factom.Entry) ([]factom.Entry, error) {
-	rand.Seed(time.Now().UnixNano())
-	for i := 1; i < len(entries); i++ {
-		r := rand.Intn(i + 1)
-		if i != r {
-			entries[r], entries[i] = entries[i], entries[r]
-		}
-	}
-	return entries, nil
-}
 
 // Grade Staking Price Records
 func (d *Pegnetd) GradeS(ctx context.Context, block *factom.EBlock) (graderStake.GradedBlock, error) {
@@ -39,19 +26,16 @@ func (d *Pegnetd) GradeS(ctx context.Context, block *factom.EBlock) (graderStake
 	if block.Height >= SprSignatureActivation {
 		ver = 6
 	}
+	if block.Height >= V202EnhanceActivation {
+		ver = 7
+	}
 
 	g, err := graderStake.NewGrader(ver, int32(block.Height))
 	if err != nil {
 		return nil, err
 	}
 
-	sprEntries := block.Entries
-	if block.Height >= V202EnhanceActivation {
-		ver = 7
-		sprEntries, _ = ShuffleEntries(block.Entries)
-	}
-
-	for _, entry := range sprEntries {
+	for _, entry := range block.Entries {
 		extids := make([][]byte, len(entry.ExtIDs))
 		for i := range entry.ExtIDs {
 			extids[i] = entry.ExtIDs[i]
