13ab301e83e38d03fc746e12222817e3df52fb72
dragonheaven <happyfamilylife11@gmail.com>
2021-03-10 21:23:17 +0100

    V204BurnMintTokenActivation

--- node/node.go
+++ node/node.go
@@ -77,8 +77,8 @@ var (
 	// Estimated to be  Mar 15th 2021
 	V204EnhanceActivation uint32 = 999999
 
-	// V204EnhanceActivation indicates the activation of PegNet 2.0.4.
-	// Estimated to be  Mar 15th 2021
+	// V204EnhanceActivation indicates the activation that burns remaining airdrop amount.
+	// Estimated to be  April 15th 2021
 	V204BurnMintTokenActivation uint32 = 999999
 )
 
